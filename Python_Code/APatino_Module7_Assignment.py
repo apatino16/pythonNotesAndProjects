870f66b18f3fb5952218bc9ee69aee501aca34b5
Anny Patino <a.a.patino16@gmail.com>
2023-10-30 14:19:13 -0700

    given rectangle class definition

---
+++ Python_Code/APatino_Module7_Assignment.py
@@ -0,0 +1,16 @@
+# Problem 1
+
+class Rectangle:
+
+    def __init__(self, width, heigth):
+        self.width = width
+        self.height = heigth
+
+    def area(self):
+        return self.width * self.height
+
+# Problem 2
+
+# Problem 3
+
+# Problem 4
