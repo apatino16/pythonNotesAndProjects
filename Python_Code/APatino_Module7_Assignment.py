ece4ed2d11ea90f63550d046284bd182b47f195c
Anny Patino <a.a.patino16@gmail.com>
2023-10-30 15:51:05 -0700

    Override the < operator

--- Python_Code/APatino_Module7_Assignment.py
+++ Python_Code/APatino_Module7_Assignment.py
@@ -1,7 +1,7 @@
-# Problem 1
+# Problem 1 and 3
 
 class Rectangle:
-    # Initializes a new instance of Rectangle with default values
+    # Problem 1: Initializes a new instance of Rectangle with default values
     def __init__(self, width, heigth, name="unnamed", x=0, y=0):
         # Initialize the attributes of the Rectangle object
         self.width = width  # Assigns the width of the rectangle
@@ -14,13 +14,23 @@ class Rectangle:
     # Calculates and returns the area of the rectangle
     def area(self):
         return self.width * self.height
-    # Generates and returns a description of the rectangle
 
+    # Problem 3: Overriding the + operator to add two rectangles together
+    def __add__(self, other):
+        new_width = self.width + other.width
+        new_heigth = self.height + other.height
+        return Rectangle(new_width, new_heigth)
+
+    # Problem 3: Overriding the < operator to compare rectangles' areas
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    # Problem 1: Generates and returns a description of the rectangle
     def description(self):
         # Return a formatted string containing details of the rectangle
         return f"Name: {self.name}, Coordinates (x, y): ({self.x}, {self.y}), Width: {self.width}, Height: {self.height}, Area: {self.area()}"
 
-     # Updates the coordinates of the rectangle to new values
+    # Problem 1: Updates the coordinates of the rectangle to new values
     def change_coords(self, new_x, new_y):
         self.x = new_x  # Updates the x-coordinate
         self.y = new_y  # Updates the y-coordinate
@@ -28,7 +38,7 @@ class Rectangle:
         print(f"Coordinates updated to ({self.x}, {self.y})")
 
 
-# Problem 2
+# Problem 2 and 4
 
 class Vehicle:
 
@@ -59,9 +69,5 @@ distance_traveled_car1 = car1.drive(30)  # Drive the car for 30 minutes
 distance_traveled_car2 = car2.drive(45)  # Drive the van for 45 minutes
 
 # Display the distance traveled and the updated odometer for each vehicle
-print(f"{car1.name} traveled {distance_traveled_car1} miles. Odometer: {car1.odometer} miles.")
-print(f"{car2.name} traveled {distance_traveled_car2} miles. Odometer: {car2.odometer} miles.")
-
-# Problem 3
-
-# Problem 4
+print(f"{car1.nameV} traveled {distance_traveled_car1} miles. Odometer: {car1.odometer} miles.")
+print(f"{car2.nameV} traveled {distance_traveled_car2} miles. Odometer: {car2.odometer} miles.")
