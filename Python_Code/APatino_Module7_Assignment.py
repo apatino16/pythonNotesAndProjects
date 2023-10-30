cdc3a20983eb1667e6fd3a00c594a5153da99163
Anny Patino <a.a.patino16@gmail.com>
2023-10-30 14:48:10 -0700

    Added a method that changes the x and y coordinates. Provided thorough comments of the code

--- Python_Code/APatino_Module7_Assignment.py
+++ Python_Code/APatino_Module7_Assignment.py
@@ -1,19 +1,32 @@
 # Problem 1
 
 class Rectangle:
-
+    # Initializes a new instance of Rectangle with default values
     def __init__(self, width, heigth, name="unnamed", x=0, y=0):
-        self.width = width
-        self.height = heigth
+        # Initialize the attributes of the Rectangle object
+        self.width = width  # Assigns the width of the rectangle
+        self.height = heigth  # Assigns the height of the rectangle
+        # Assigns a name to the rectangle (defaults to "unnamed")
         self.name = name
-        self.x = x
-        self.y = y
+        self.x = x  # Assigns the x-coordinate (defaults to 0)
+        self.y = y  # Assigns the y-coordinate (defaults to 0)
 
+    # Calculates and returns the area of the rectangle
     def area(self):
         return self.width * self.height
+    # Generates and returns a description of the rectangle
 
     def description(self):
-        print(f"The object's name is {self.name}, the x equals to {self.x} and the y equals to {self.y}. The width equalts to {self.width} and the heigth equalts to {self.height}, given this the area is equal to {self.area}")
+        # Return a formatted string containing details of the rectangle
+        return f"Name: {self.name}, Coordinates (x, y): ({self.x}, {self.y}), Width: {self.width}, Height: {self.height}, Area: {self.area()}"
+
+     # Updates the coordinates of the rectangle to new values
+    def change_coords(self, new_x, new_y):
+        self.x = new_x  # Updates the x-coordinate
+        self.y = new_y  # Updates the y-coordinate
+        # Print confirmation of updated coordinates
+        print(f"Coordinates updated to ({self.x}, {self.y})")
+
 
 # Problem 2
 
